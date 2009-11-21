4c38627e4562a8a88a3e31e3fe4fe310f411b04b
davisjt <jaynsilentbob@gmail.com>
2009-11-21 13:27:50 -0500

    Added new files

---
+++ src/main/java/edu/bu/Colors.java
@@ -0,0 +1,5 @@
+package edu.bu;
+
+public enum Colors {
+	Uncolored, Red, Blue, Green;
+}
---
+++ src/main/java/edu/bu/Player.java
@@ -0,0 +1,14 @@
+package edu.bu;
+
+
+public class Player {
+	private AtroposState currentstate;
+	
+	Player(AtroposState state) {
+		this.currentstate = state;	
+	}
+	
+	private void EvaluateMove() {
+		
+	}
+}
